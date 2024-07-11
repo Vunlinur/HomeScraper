9627ccf06d2c9fbd05292979e9ece4fef5990ea7
Vunlinur <mightymetalfan@gmail.com>
2024-07-11 21:52:40 +0200

    Fix OtoDom provider

--- HomeScraper/Providers/Otodom.cs
+++ HomeScraper/Providers/Otodom.cs
@@ -1,15 +1,11 @@
 ﻿using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace HomeScraper.Providers {
 	[Provider]
 	class Otodom : IProvider {
-		const string valueCellClass = ".//div[@class='css-1wi2w6s estckra5']";
+		const string valueCellClass = ".//div[@class='css-1wi2w6s']";
 		const string stripNonNumbers = @"[^,\d]";
 		// TODO inherit these fields
 		const string providerName = "otodom";
@@ -28,15 +24,14 @@ namespace HomeScraper.Providers {
 
 			homeData.Address = doc.DocumentNode.SelectSingleNode("//a[@aria-label='Adres']").InnerText;
 
-			var detailTable = doc.DocumentNode.SelectSingleNode("//div[@data-testid='ad.top-information.table']");
-
-			var area = detailTable.SelectSingleNode("//div[@aria-label='Powierzchnia']").SelectSingleNode(valueCellClass).InnerText;
+			var area = doc.DocumentNode.SelectSingleNode("//*[@id=\"__next\"]/main/div[2]/div[2]/div[1]/div/div[1]/div[2]/div").InnerText;
 			homeData.Area = float.Parse(Regex.Replace(area, stripNonNumbers, ""));
 			
-			var landArea = detailTable.SelectSingleNode("//div[@aria-label='Powierzchnia działki']").SelectSingleNode(valueCellClass).InnerText;
-			homeData.LandArea = float.Parse(Regex.Replace(landArea, stripNonNumbers, ""));
+			var landArea = doc.DocumentNode.SelectSingleNode("//div[@aria-label='Powierzchnia działki']")?.InnerText;
+			if (landArea is not null)
+				homeData.LandArea = float.Parse(Regex.Replace(landArea, stripNonNumbers, ""));
 
-			var rooms = detailTable.SelectSingleNode("//div[@aria-label='Liczba pokoi']").SelectSingleNode(valueCellClass).InnerText;
+			var rooms = doc.DocumentNode.SelectSingleNode("//*[@id=\"__next\"]/main/div[2]/div[2]/div[1]/div/div[3]/div[2]/div/a").InnerText;
 			homeData.Rooms = int.Parse(Regex.Replace(rooms, stripNonNumbers, ""));
 
 			return homeData;
